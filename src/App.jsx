7a5f55677bea0f145f25d480c865fe6d1ff492ae
Daudyyy <chaudharydawood68@gmail.com>
2024-09-06 00:58:27 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Card from "./components/Card";
+function App() {
+  return (
+    <div
+      className="bg-violet-500 h-screen w-screen"
+    >
+      <div className="container mx-auto flex items-center justify-center h-screen">
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
